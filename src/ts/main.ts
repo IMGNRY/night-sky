bb72883a18dd2e9f7315177598c691d23d208ae8
Fille <fille@imgnry.com>
2019-11-25 13:59:01 +0100

    pixijs ftw!

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,283 +1,203 @@
 import { sleep, random } from './tools'
 import './tools'
-import anime from 'animejs'
-
-// interface Vector {
-//     x: number
-//     y: number
-// }
-
-// interface Particle extends HTMLDivElement {
-//     createdAt: number
-//     pos: Vector
-//     scale: Vector
-//     opacity: number
-//     velocity: Vector
-//     rotation: number
-// }
-
-// const applyChanges = (particle: Particle) => {
-//     particle.style.transform = `rotate(${particle.rotation}deg) translate3d(${particle.pos.x}px, ${particle.pos.y}px, 0) scale(${particle.scale.x}, ${particle.scale.y})`
-//     particle.style.opacity = `${particle.opacity}`
-// }
-// const applyPosition = (particle: Particle) => {
-//     particle.style.transform = `translate3d(${particle.pos.x}px, ${particle.pos.y}px, 0)`
-// }
-
-// const stars: Particle[] = []
-// const meteors: Particle[] = []
-// const easing = Bezier(0, 0.55, 0.67, 0.59)
+import * as PIXI from 'pixi.js'
+import { RGBSplitFilter } from '@pixi/filter-rgb-split'
+import { AdvancedBloomFilter } from '@pixi/filter-advanced-bloom'
 
 const PREF = {
-    STAR_TTL: 10000,
-    STAR_SCALE_RANGE: [0.3, 1.5],
-    STAR_BIRTH_INTERVAL: 300,
-    METEOR_TTL: 4000,
-    METEOR_BIRTH_INTERVAL: 1000
+    STAR_MAX_ALPHA_VELOCITY: 0.01,
+    METEOR_VELOCITY: 15,
+    // STAR_SCALE_RANGE: [0.3, 1.5],
+    STAR_BIRTH_INTERVAL: 50,
+    STAR_COUNT: 200
+    // METEOR_TTL: 4000,
+    // METEOR_BIRTH_INTERVAL: 1000
 }
 
-// create stars
-
-// create stars
-// setInterval(() => {
-//     const star = document.createElement('div') as Particle
-//     star.classList.add('star')
-//     star.createdAt = TS
-//     star.pos = {
-//         x: random(0, window.innerWidth),
-//         y: random(0, window.innerHeight)
-//     }
-//     const scale = random(PREF.STAR_SCALE_RANGE[0], PREF.STAR_SCALE_RANGE[1])
-//     star.scale = {
-//         x: scale,
-//         y: scale
-//     }
-//     star.rotation = 0
-//     star.opacity = 0
-//     stars.push(star)
-//     document.body.appendChild(star)
-// }, PREF.STAR_BIRTH_INTERVAL)
-
-// // create meteors
-// setInterval(() => {
-const randomScreenX = () => Math.random().remap([0, 1], [0, winSize.w])
-const randomScreenY = () => Math.random().remap([0, 1], [0, winSize.h])
-const randomRotation = () => Math.random().remap([0, 1], [-60, 60])
+// const randomRotation = () => Math.random().remap([0, 1], [-60, 60])
 
 let winSize: { w: number; h: number } = { w: 0, h: 0 }
-let nightSkyContainer: HTMLDivElement
+// let nightSkyContainer: HTMLDivElement
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
     console.log('DOMContentLoaded')
 
-    winSize = {
-        w: window.innerWidth,
-        h: window.innerHeight
+    // winSize = {
+    //     w: window.innerWidth,
+    //     h: window.innerHeight
+    // }
+
+    const nightSkyContainer = document.querySelector('.night-sky-container')
+    const containerRect = nightSkyContainer.getBoundingClientRect()
+    const app = new PIXI.Application({
+        backgroundColor: 0x0b1011,
+        width: containerRect.width,
+        height: containerRect.height,
+        antialias: true,
+        // transparent: true,
+        autoDensity: true,
+        forceFXAA: true
+    })
+    app.stage.filters = [
+        new RGBSplitFilter(new PIXI.Point(-0.5, -0.5), new PIXI.Point(1, -0.5), new PIXI.Point(-1, 0.5)),
+        new AdvancedBloomFilter({ threshold: 0.3, blur: 6, bloomScale: 2, brightness: 1 })
+    ]
+    nightSkyContainer.appendChild(app.view)
+
+    const randomScreenX = () => Math.random().remap([0, 1], [0, app.renderer.width])
+    const randomScreenY = () => Math.random().remap([0, 1], [0, app.renderer.height])
+
+    interface Star extends PIXI.Sprite {
+        alphaVelocity: number
     }
 
-    nightSkyContainer = document.querySelector('.night-sky-container')
+    const resources = await new Promise<Partial<Record<string, PIXI.LoaderResource>>>(resolve => {
+        app.loader.add('meteor', 'meteor.png')
+        app.loader.add('star', 'star.png', {})
+        app.loader.add('cloud', 'cloud1.png')
+        app.loader.load((loader, resources) => {
+            resolve(resources)
+        })
+    })
 
-    const createstarsIntervalId = setInterval(() => {
-        const star = document.createElement('div')
-        star.classList.add('star')
-        nightSkyContainer.appendChild(star)
-        star.style.transform = `translate3d(${randomScreenX()}px,${randomScreenY()}px, 0)`
+    // app.loader.add('meteor', 'meteor.png').load((loader, resources) => {
+    const stars: Star[] = []
+    const particleContainer = new PIXI.ParticleContainer(300, { tint: true })
+    app.stage.addChild(particleContainer)
 
-        const starAnim = anime({
-            targets: star,
-            opacity: 1,
-            duration: 3000,
-            direction: 'alternate',
-            loop: 2,
-            easing: 'linear',
-            complete: () => {
-                // console.log('complete')
-                star.style.transform = `translate3d(${randomScreenX()}px,${randomScreenY()}px, 0)`
-                starAnim.restart()
+    const starBirthIntervalId = setInterval(() => {
+        const star = new PIXI.Sprite(resources['star'].texture) as Star
+        // let star = new PIXI.Graphics() as Star
+        // star.beginFill(0xffffff)
+        // star.drawCircle(0, 0, random(0.5, 1))
+        // star.endFill()
+
+        star.anchor.set(0.5)
+
+        star.x = randomScreenX()
+        star.y = randomScreenY()
+
+        star.alphaVelocity = random(-PREF.STAR_MAX_ALPHA_VELOCITY, -PREF.STAR_MAX_ALPHA_VELOCITY)
+        // star.tint = 0xff0000
+        star.alpha = 0
+        star.scale.set(0)
+
+        particleContainer.addChild(star)
+        stars.push(star)
+        if (stars.length == PREF.STAR_COUNT) {
+            clearTimeout(starBirthIntervalId)
+        }
+    }, PREF.STAR_BIRTH_INTERVAL)
+
+    const meteorAnchor = new PIXI.Container()
+    meteorAnchor.x = 200
+    meteorAnchor.angle = -70
+
+    // const meteor = new PIXI.Sprite(resources.meteor.texture)
+    const meteor = new PIXI.Graphics()
+    const gradient = createGradient('rgba(95, 220, 255, 0)', 'rgba(255, 255, 255, 255)')
+    meteor.beginTextureFill({ texture: gradient })
+    meteor.drawRect(0, 0, 4, 100)
+    meteor.endFill()
+
+    // meteor.scale.x = 0.5
+    meteor.scale.y = 3
+    meteor.y = 100
+    // meteor.x = randomScreenX()
+    // meteor.y = randomScreenY()
+
+    // star.alphaVelocity = random(-PREF.STAR_MAX_ALPHA_VELOCITY, -PREF.STAR_MAX_ALPHA_VELOCITY)
+    // star.alpha = 0.001
+
+    meteorAnchor.addChild(meteor)
+    app.stage.addChild(meteorAnchor)
+
+    const cloud = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    cloud.width = app.renderer.width
+    cloud.y -= 150
+    cloud.scale.set(1.5)
+    app.stage.addChild(cloud)
+
+    const cloud_ = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    cloud_.width = app.renderer.width
+    cloud_.y -= 50
+    cloud_.tilePosition.x += 300
+    app.stage.addChild(cloud_)
+
+    const cloud2 = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    cloud2.width = app.renderer.width
+    // cloud2.y += 100
+    cloud2.scale.set(1.5)
+    cloud2.tilePosition.x += 200
+    app.stage.addChild(cloud2)
+
+    const cloud3 = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    cloud3.width = app.renderer.width
+    cloud3.y += 100
+    cloud3.alpha = 0.4
+    cloud3.scale.set(2.5)
+    cloud3.tilePosition.x += 400
+    app.stage.addChild(cloud3)
+
+    const cloud4 = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    cloud4.width = app.renderer.width
+    // cloud4.y += 200
+    cloud4.alpha = 0.3
+    cloud4.scale.set(3)
+    cloud4.tilePosition.x += 500
+    app.stage.addChild(cloud4)
+
+    app.ticker.add(deltaTime => {
+        // console.log('deltaTime:', deltaTime)
+
+        stars.forEach(star => {
+            if (star.alpha >= 1) {
+                star.alphaVelocity = -star.alphaVelocity
+            } else if (star.alpha <= 0) {
+                star.x = randomScreenX()
+                star.y = randomScreenY()
+                star.alphaVelocity = -star.alphaVelocity
             }
-            // loopComplete: () => {
-            //     console.log('loopComplete')
-            // }
+            star.alpha += star.alphaVelocity
+            star.scale.set(star.scale.x + star.alphaVelocity * 0.5)
         })
 
-        // console.log('star anim:', starAnim)
-    }, 1000)
+        meteor.y += PREF.METEOR_VELOCITY
+        meteor.scale.y -= 0.01
+        meteor.scale.x *= 0.99
+        meteor.alpha *= 0.99
 
-    // meteorAnchor.classList.add('meteor-anchor')
-    // meteorAnchor.style.transform = `translateX(${randomScreenX()}px) rotate(${randomRotation()}deg)`
-    // const meteor = document.createElement('div')
-    // meteor.classList.add('meteor', 'meteor-anim')
-    // meteorAnchor.appendChild(meteor)
-    // nightSkyContainer.appendChild(meteorAnchor)
-    // document.body.insertBefore(nightSkyContainer, document.body.firstChild)
-    // meteor.addEventListener('animationend', async ev => {
-    //     console.log('animationend')
+        if (meteor.alpha < 0.1) {
+            meteor.y = -meteor.height * 3
+            meteor.scale.y = 3
+            meteor.scale.x = 1
+            meteor.alpha = 1
+            if (random(0, 1) > 0.5) {
+                meteorAnchor.x = random(-200, app.renderer.width / 2 - 200)
+                meteorAnchor.angle = -random(55, 80)
+            } else {
+                meteorAnchor.x = random(app.renderer.width / 2 + 200, app.renderer.width + 200)
+                meteorAnchor.angle = random(55, 80)
+            }
+        }
 
-    //     // const meteorClone = meteor.cloneNode(true) as HTMLDivElement
-    //     // meteorClone.classList.add('hej')
-
-    //     // meteor.style.animation = ''
-    //     meteor.classList.remove('meteor-anim')
-    //     meteorAnchor.style.transform = `translate3d(${randomScreenX()}px, 0, 0) rotate(${randomRotation()}deg)`
-    //     await sleep(random(300, 3000))
-    //     // meteor.replaceWith(meteorClone)
-
-    //     // meteor.parentNode.replaceChild(meteorClone, meteor)
-
-    //     // console.log('done')
-
-    //     // meteor.style.animation = 'meteor'
-    //     // setTimeout(() => {
-    //     meteor.classList.add('meteor-anim')
-    //     // }, 1000)
-    // })
-
-    // const createstarsIntervalId = setInterval(() => {
-    //     const star = document.createElement('div') as Particle
-    //     star.classList.add('star', 'star-anim')
-    //     star.addEventListener('animationend', async ev => {
-    //         star.pos = {
-    //             x: random(0, winSize.w),
-    //             y: random(0, winSize.h)
-    //         }
-    //         // star.style.animation = ''
-    //         star.classList.remove('star-anim')
-    //         await sleep(0)
-    //         // star.style.animation = 'star'
-    //         star.classList.add('star-anim')
-    //         applyPosition(star)
-    //     })
-    //     // star.createdAt = TS
-    //     star.pos = {
-    //         x: random(0, winSize.w),
-    //         y: random(0, winSize.h)
-    //     }
-    //     // const scale = random(PREF.STAR_SCALE_RANGE[0], PREF.STAR_SCALE_RANGE[1])
-    //     // star.scale = {
-    //     //     x: 1,
-    //     //     y: 1
-    //     // }
-    //     // star.rotation = 0
-    //     // star.opacity = 1
-    //     applyPosition(star)
-    //     stars.push(star)
-    //     nightSkyContainer.appendChild(star)
-    //     if (stars.length >= 10) {
-    //         clearInterval(createstarsIntervalId)
-    //     }
-    // }, 200)
+        cloud.tilePosition.x -= 0.2
+        cloud_.tilePosition.x = 0.2
+        cloud2.tilePosition.x += 0.4
+        cloud3.tilePosition.x += 0.8
+        cloud4.tilePosition.x += 1
+    })
 })
-//     // const meteor = document.createElement('div') as Particle
-//     // meteors.push(meteor)
-//     // document.body.appendChild(meteor)
-//     const anim = anime({
-//         targets: '.meteor',
-
-//         // duration: 3000,
-//         keyframes: [
-//             {
-//                 // rotate: -5,
-//                 // translateX: window.innerWidth / 2,
-//                 translateY: -500,
-//                 opacity: 0,
-//                 duration: 0
-//             },
-//             {
-//                 // translateY: 250,
-//                 opacity: 1,
-//                 duration: 200
-//             },
-//             {
-//                 // translateY: 250,
-//                 opacity: 1,
-//                 duration: 1500
-//             },
-//             {
-//                 translateY: 700,
-//                 opacity: 0,
-//                 duration: 1500,
-//                 scale: 0.5
-//             }
-//         ],
-//         loop: true,
-//         easing: 'easeOutCubic',
-//         loopComplete: () => {
-//             console.log('done')
-
-//             // const i = meteors.findIndex(m => m == meteor)
-//             // meteors.splice()
-//             // setTin
-//         }
-//     })
-//     // anim.play()
-
-//     console.log('tut')
 // })
 
-// meteor.classList.add('meteor')
-// star.addEventListener('animationend', async ev => {})
-// // meteor.createdAt = TS
-// // meteor.pos = { x: random(0, window.innerWidth), y: -100 }
-// // meteor.scale = { x: 1, y: 5 }
-// // meteor.velocity = { x: 0, y: 8 }
-// // meteor.rotation = random(-45, 45)
-// }, PREF.METEOR_BIRTH_INTERVAL)
-
-// let TS = 0
-// const tick = (now: number) => {
-//     TS = now
-
-//     // stars
-//     // for (let i = stars.length - 1; i >= 0; i--) {
-//     //     const star = stars[i]
-//     //     const age = TS - star.createdAt
-
-//     // remove old stars
-//     // if (age >= PREF.STAR_TTL) {
-//     //     stars.splice(i, 1)
-//     //     star.classList.remove('star-anim')
-//     //     star.style.opacity = '0'
-//     //     // star.remove()
-//     // }
-
-//     // let opacity = easing(age.remap([0, PREF.STAR_TTL], [0, 1]))
-//     // let opacity = 0
-//     // const TTL_HALF = PREF.STAR_TTL * 0.5
-//     // if (age < TTL_HALF) {
-//     //     opacity = age.remap([0, TTL_HALF], [0, 1])
-//     // } else {
-//     //     opacity = age.remap([TTL_HALF, PREF.STAR_TTL], [1, 0])
-//     // }
-//     // star.opacity = opacity
-//     // render(star)
-//     // }
-
-//     // meteors
-//     for (let i = meteors.length - 1; i >= 0; i--) {
-//         const meteor = meteors[i]
-//         const age = TS - meteor.createdAt
-
-//         // remove old stars
-//         if (age >= PREF.METEOR_TTL) {
-//             meteors.splice(i, 1)
-//             meteor.remove()
-//         }
-
-//         // apply velocity
-//         meteor.pos.x += meteor.velocity.x
-//         meteor.pos.y += meteor.velocity.y
-
-//         // shrink
-//         meteor.scale.y *= 0.99
-
-//         // reduce velocity
-//         meteor.velocity.x *= 0.99
-//         meteor.velocity.y *= 0.99
-
-//         let opacity = age.remap([0, PREF.METEOR_TTL], [1, 0])
-//         meteor.opacity = opacity
-//         applyChanges(meteor)
-//     }
-//     requestAnimationFrame(tick)
-// }
-// requestAnimationFrame(tick)
+function createGradient(from: string, to: string) {
+    const c = document.createElement('canvas')
+    const ctx = c.getContext('2d')
+    const grd = ctx.createLinearGradient(0, 0, 0, 100)
+    grd.addColorStop(0, from)
+    // grd.addColorStop(0.5, from)
+    grd.addColorStop(1, to)
+    ctx.fillStyle = grd
+    ctx.fillRect(0, 0, 100, 100)
+    return PIXI.Texture.from(c)
+}
