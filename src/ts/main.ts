6f344f41e7cdd248a67bd7a71d132ea5e5c9b4fe
Fille <fille@imgnry.com>
2019-11-27 01:11:35 +0100

    cleanup

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -20,11 +20,35 @@ window.addEventListener('DOMContentLoaded', async () => {
         transparent: true,
         width: containerRect.width,
         height: containerRect.height,
-        antialias: true,
-        autoDensity: true,
-        forceFXAA: true
+        antialias: true
+        // resolution: 2,
+        // autoDensity: true,
+        // forceFXAA: true
     })
+    nightSkyContainer.appendChild(app.view)
 
+    const bg = new PIXI.Container()
+    app.stage.addChild(bg)
+
+    // screen fx
+    bg.filters = [
+        new RGBSplitFilter(new PIXI.Point(-0.5, -0.5), new PIXI.Point(1, -0.5), new PIXI.Point(-1, 0.5)),
+        new AdvancedBloomFilter({ threshold: 0.6, blur: 3, bloomScale: 4, brightness: 1 })
+    ]
+
+    // fade in
+    app.stage.alpha = 0
+    const starFieldBgDiv = document.querySelector('.starfield-background') as HTMLDivElement
+    const revealStarFieldBgDiv = () => {
+        const opacity = Number(starFieldBgDiv.style.opacity)
+        starFieldBgDiv.style.opacity = `${opacity + 0.005}`
+        if (opacity < 1) {
+            requestAnimationFrame(revealStarFieldBgDiv)
+        }
+    }
+    requestAnimationFrame(revealStarFieldBgDiv)
+
+    // react to windows resize
     window.addEventListener('resize', () => {
         const containerRect = nightSkyContainer.getBoundingClientRect()
         app.renderer.resize(containerRect.width, containerRect.height)
@@ -35,32 +59,18 @@ window.addEventListener('DOMContentLoaded', async () => {
         cloud4.width = app.renderer.width
     })
 
-    app.stage.filters = [
-        new RGBSplitFilter(new PIXI.Point(-0.5, -0.5), new PIXI.Point(1, -0.5), new PIXI.Point(-1, 0.5)),
-        new AdvancedBloomFilter({ threshold: 0.6, blur: 3, bloomScale: 4, brightness: 1 })
-    ]
-    nightSkyContainer.appendChild(app.view)
-    app.stage.alpha = 0
-
     const randomScreenX = () => Math.random().remap([0, 1], [0, app.renderer.width])
     const randomScreenY = () => Math.random().remap([0, 1], [0, app.renderer.height])
 
+    // stars
     interface Star extends PIXI.Sprite {
         alphaVelocity: number
         scaleMax: number
     }
-
-    const resources = await new Promise<Partial<Record<string, PIXI.LoaderResource>>>(resolve => {
-        app.loader.add('star', '/textures/star.png', {})
-        app.loader.add('cloud', '/textures/cloud.png')
-        app.loader.load((loader, resources) => {
-            resolve(resources)
-        })
-    })
-
+    const starTexture = PIXI.Texture.from('/textures/star.png')
     const stars: Star[] = []
     const starBirthIntervalId = setInterval(() => {
-        const star = new PIXI.Sprite(resources['star'].texture) as Star
+        const star = new PIXI.Sprite(starTexture) as Star
         star.anchor.set(0.5)
         star.x = randomScreenX()
         star.y = randomScreenY()
@@ -68,13 +78,14 @@ window.addEventListener('DOMContentLoaded', async () => {
         star.alpha = 0
         star.scaleMax = random(0.3, 1)
         star.scale.set(0)
-        app.stage.addChild(star)
+        bg.addChild(star)
         stars.push(star)
         if (stars.length == PREF.STAR_COUNT) {
             clearTimeout(starBirthIntervalId)
         }
     }, PREF.STAR_BIRTH_INTERVAL)
 
+    // meteor
     const meteorAnchor = new PIXI.Container()
     meteorAnchor.x = 200
     meteorAnchor.angle = -70
@@ -86,49 +97,67 @@ window.addEventListener('DOMContentLoaded', async () => {
     meteor.endFill()
     meteor.scale.y = PREF.METEOR_START_LENGTH_MOD
     meteor.y = -meteor.height
+    meteor.visible = false
     meteorAnchor.addChild(meteor)
+    app.stage.addChild(meteorAnchor)
     setTimeout(() => {
-        app.stage.addChild(meteorAnchor)
+        meteor.visible = true
     }, 3000)
-    const cloud = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+
+    // const logo = PIXI.Sprite.from('/textures/logo.svg')
+    // logo.anchor.set(0.5)
+    // logo.x = app.renderer.width / 2 / 2
+    // logo.y = app.renderer.height / 2 / 2
+    // logo.filters = [
+    //     new RGBSplitFilter(new PIXI.Point(-0.5, -0.5), new PIXI.Point(0.5, -0.5), new PIXI.Point(-0.5, 0.5))
+    //     // new AdvancedBloomFilter({ threshold: 0.6, blur: 0, bloomScale: 1, brightness: 0.5 })
+    // ]
+    // app.stage.addChild(logo)
+
+    // clouds
+    const cloudTexture = PIXI.Texture.from('/textures/cloud.png')
+    const cloud = new PIXI.TilingSprite(cloudTexture, 600, 276)
     cloud.width = app.renderer.width
     cloud.y -= 150
     cloud.scale.set(1.5)
-    app.stage.addChild(cloud)
+    bg.addChild(cloud)
 
-    const cloud_ = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    const cloud_ = new PIXI.TilingSprite(cloudTexture, 600, 276)
     cloud_.width = app.renderer.width
     cloud_.y -= 50
     cloud_.tilePosition.x += 300
-    app.stage.addChild(cloud_)
+    bg.addChild(cloud_)
 
-    const cloud2 = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    const cloud2 = new PIXI.TilingSprite(cloudTexture, 600, 276)
     cloud2.width = app.renderer.width
     cloud2.scale.set(1.5)
     cloud2.tilePosition.x += 200
-    app.stage.addChild(cloud2)
+    bg.addChild(cloud2)
 
-    const cloud3 = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    const cloud3 = new PIXI.TilingSprite(cloudTexture, 600, 276)
     cloud3.width = app.renderer.width
     cloud3.y += 100
     cloud3.alpha = 0.4
     cloud3.scale.set(2.5)
     cloud3.tilePosition.x += 400
-    app.stage.addChild(cloud3)
+    bg.addChild(cloud3)
 
-    const cloud4 = new PIXI.TilingSprite(resources.cloud.texture, 600, 276)
+    const cloud4 = new PIXI.TilingSprite(cloudTexture, 600, 276)
     cloud4.width = app.renderer.width
     cloud4.alpha = 0.3
     cloud4.scale.set(3)
     cloud4.tilePosition.x += 500
-    app.stage.addChild(cloud4)
+    bg.addChild(cloud4)
 
+    // ticker
     app.ticker.add(() => {
+        // fade in
         if (app.stage.alpha < 1) {
             app.stage.alpha += 0.005
             if (app.stage.alpha > 1) app.stage.alpha = 1
         }
 
+        // star
         stars.forEach(star => {
             if (star.alpha >= 1) {
                 star.alphaVelocity = -star.alphaVelocity
@@ -141,6 +170,7 @@ window.addEventListener('DOMContentLoaded', async () => {
             star.scale.set(Math.min(star.scale.x + star.alphaVelocity, star.scaleMax))
         })
 
+        // meteor
         if (meteor.visible) {
             meteor.y += PREF.METEOR_VELOCITY
             meteor.scale.y *= 0.98
@@ -167,6 +197,7 @@ window.addEventListener('DOMContentLoaded', async () => {
             }
         }
 
+        // clouds
         cloud.tilePosition.x -= 0.2
         cloud_.tilePosition.x = 0.2
         cloud2.tilePosition.x += 0.4
